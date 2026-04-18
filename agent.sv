7b266a3551ac75b74661dfbb8d054e15a7c313b7
Dansalas <desteban2201@gmail.com>
2026-04-18 14:12:10 -0600

    Separacion del agente/generador, Modificacion del ambiente

--- agent.sv
+++ agent.sv
@@ -7,15 +7,15 @@
 //    Específica: en este tipo se generan trasacciones semi específicas para casos esquina      // 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-class agent #(parameter width = 16, parameter depth = 8);
-  trans_fifo_mbx agnt_drv_mbx;           // Mailbox del agente al driver
-  comando_test_agent_mbx test_agent_mbx; // Mailbox del test al agente
-  int num_transacciones;                 // Número de transacciones para las funciones del agente
+class generador #(parameter width = 16, parameter depth = 8);
+  trans_fifo_mbx gen_agent_mbx;          // Mailbox del generador al agente
+  comando_test_gen_mbx test_gen_mbx;    // Mailbox del test al generador
+  int num_transacciones;                 // Número de transacciones para las funciones del generador
   int max_retardo;                       // retardo máximo en la transacción
   int ret_spec;                          // retardo específico
   tipo_trans tpo_spec; 
   bit [width-1:0] dto_spec;
-  instrucciones_agente instruccion;      // para guardar la última instruccion leída
+  instrucciones_gen instruccion;      // para guardar la última instruccion leída
   trans_fifo #(.width(width)) transaccion;
    
   function new;
@@ -24,12 +24,12 @@ class agent #(parameter width = 16, parameter depth = 8);
   endfunction
 
   task run;
-    $display("[%g]  El Agente fue inicializado",$time);
+    $display("[%g]  El generador fue inicializado",$time);
     forever begin
       #1
-      if(test_agent_mbx.num() > 0)begin
-        $display("[%g]  Agente: se recibe instruccion",$time);
-        test_agent_mbx.get(instruccion);
+      if(test_gen_mbx.num() > 0)begin
+        $display("[%g]  Generador: se recibe instruccion",$time);
+        test_gen_mbx.get(instruccion);
         case(instruccion)
           llenado_aleatorio: begin  // Esta instruccion genera num_transacciones escrituras seguidas del mismo número de lecturas
             for(int i = 0; i < num_transacciones;i++) begin
@@ -38,44 +38,66 @@ class agent #(parameter width = 16, parameter depth = 8);
               transaccion.randomize();
               tpo_spec = escritura;
               transaccion.tipo = tpo_spec;
-              transaccion.print("Agente: transacción creada");
-              agnt_drv_mbx.put(transaccion);
+              transaccion.print("Generador: transacción creada");
+              gen_agent_mbx.put(transaccion);
             end
             for(int i=0; i<num_transacciones;i++) begin
               transaccion =new;
               transaccion.randomize();
               tpo_spec = lectura;
               transaccion.tipo = tpo_spec;
-              transaccion.print("Agente: transacción creada");
-              agnt_drv_mbx.put(transaccion);
+              transaccion.print("Generador: transacción creada");
+              gen_agent_mbx.put(transaccion);
             end
           end
           trans_aleatoria: begin  // Esta instrucción genera una transaccion aleatoria
             transaccion =new;
             transaccion.max_retardo = max_retardo;
             transaccion.randomize();
-            transaccion.print("Agente: transacción creada");
-            agnt_drv_mbx.put(transaccion);
+            transaccion.print("Generador: transacción creada");
+            gen_agent_mbx.put(transaccion);
           end
           trans_especifica: begin  // Esta instrucción genera una transacción específica
             transaccion =new;
             transaccion.tipo = tpo_spec;
             transaccion.dato = dto_spec;
             transaccion.retardo = ret_spec;
-            transaccion.print("Agente: transacción creada");
-            agnt_drv_mbx.put(transaccion);
+            transaccion.print("Generador: transacción creada");
+            gen_agent_mbx.put(transaccion);
           end
           sec_trans_aleatorias: begin // Esta instrucción genera una secuencia de instrucciones aleatorias
             for(int i=0; i<num_transacciones;i++) begin
             transaccion =new;
             transaccion.max_retardo = max_retardo;
             transaccion.randomize();
-            transaccion.print("Agente: transacción creada");
-            agnt_drv_mbx.put(transaccion);
+            transaccion.print("Generador: transacción creada");
+            gen_agent_mbx.put(transaccion);
             end
           end
         endcase
       end
     end
+  endtask
+endclass
+
+class agent #(parameter  width = 16, parameter depth = 8);
+  trans_fifo_mbx agent_drv_mbx;
+  trans_fifo_mbx agent_scrb_mbx;
+  trans_fifo_mbx gen_agent_mbx;
+  trans_fifo #(.width(width)) transaccion;
+
+  task run;
+  $display("[%g]  El agente fue inicializado", $time);
+  forever  begin
+    #1
+    if(gen_agent_mbx.num() > 0) begin
+      $display("[%g]  Agente: se recibe instruccion desde generador",$time);
+      gen_agent_mbx.get(transaccion);
+      agent_scrb_mbx.put(transaccion);
+      $display("[%g]  Agente: se entrega instruccion al scoreboard",$time);
+      agent_drv_mbx.put(transaccion);
+      $display("[%g]  Agente: se entrega instruccion al driver",$time);
+    end
+  end
   endtask
 endclass
--- ambiente.sv
+++ ambiente.sv
@@ -7,41 +7,50 @@ class ambiente #(parameter width =16, parameter depth = 8);
   driver #(.width(width)) driver_inst;
   checker_c #(.width(width),.depth(depth)) checker_inst;
   score_board #(.width(width)) scoreboard_inst;
-  agent #(.width(width),.depth(depth)) agent_inst;
+  agent #(.width(width),.depth(depth)) agent_inst; //*
+  generador #(.width(width),.depth(depth)) gen_inst; //*
   
   // Declaración de la interface que conecta el DUT 
   virtual fifo_if  #(.width(width)) _if;
 
   //declaración de los mailboxes
-  trans_fifo_mbx agnt_drv_mbx;           //mailbox del agente al driver
+  trans_fifo_mbx agent_drv_mbx;           //mailbox del agente al driver *
   trans_fifo_mbx drv_chkr_mbx;           //mailbox del driver al checher
+  trans_fifo_mbx gen_agent_mbx;           //mailboox del generador al agente *
+  trans_fifo_mbx agent_scrb_mbx;          //mailbox del agente al scoreboard *
   trans_sb_mbx chkr_sb_mbx;              //mailbox del checker al scoreboard
   comando_test_sb_mbx test_sb_mbx;       //mailbox del test al scoreboard
-  comando_test_agent_mbx test_agent_mbx; //mailbox del test al agente
+  comando_test_agent_mbx test_gen_mbx; //mailbox del test al generador *
 
   function new();
     // Instanciación de los mailboxes
     drv_chkr_mbx   = new();
-    agnt_drv_mbx   = new();
+    agent_drv_mbx   = new(); //*
     chkr_sb_mbx    = new();
     test_sb_mbx    = new();
-    test_agent_mbx = new();
+    test_gen_mbx   = new(); //*
+    gen_agent_mbx  = new(); //*
+    agent_scrb_mbx = new(); //*
 
     // instanciación de los componentes del ambiente
     driver_inst     = new();
     checker_inst    = new();
     scoreboard_inst = new();
     agent_inst      = new();
+    gen_inst        = new(); //*
     // conexion de las interfaces y mailboxes en el ambiente
     driver_inst.vif             = _if;
     driver_inst.drv_chkr_mbx    = drv_chkr_mbx;
-    driver_inst.agnt_drv_mbx    = agnt_drv_mbx;
+    driver_inst.agnt_drv_mbx    = agent_drv_mbx;
     checker_inst.drv_chkr_mbx   = drv_chkr_mbx;
     checker_inst.chkr_sb_mbx    = chkr_sb_mbx;
     scoreboard_inst.chkr_sb_mbx = chkr_sb_mbx;
     scoreboard_inst.test_sb_mbx = test_sb_mbx;
-    agent_inst.test_agent_mbx   = test_agent_mbx;
-    agent_inst.agnt_drv_mbx = agnt_drv_mbx;
+    gen_inst.test_gen_mbx   = test_gen_mbx; //*
+    gen_inst.gen_agent_mbx = gen_agent_mbx; //*
+    agent_inst.agent_drv_mbx = agent_drv_mbx; //*
+    agent_inst.agent_scrb_mbx = agent_scrb_mbx; //*
+    agent_inst.gen_agent_mbx = gen_agent_mbx; //*
   endfunction
 
   virtual task run();
@@ -51,6 +60,7 @@ class ambiente #(parameter width =16, parameter depth = 8);
       checker_inst.run();
       scoreboard_inst.run();
       agent_inst.run();
+      gen_inst.run();
     join_none
   endtask 
 endclass
